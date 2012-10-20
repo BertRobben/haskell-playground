49e884e5079a64fc109db685bc800308ccc05315
Bert <bert.robben@gmail.com>
2012-10-20 22:42:44 +0200

    added weighted sum

--- search/src/Abc.hs
+++ search/src/Abc.hs
@@ -19,14 +19,13 @@ module Abc (
 import Domain
 
 abcConstraint :: String -> Int -> Constraint
-abcConstraint s n d = let (min,max) = eval s d in
-    if min > n || max < n then [] else d
+abcConstraint s n d = undefined
 
 value :: Char -> Domain -> Int
 value = undefined
 
 eval :: String -> Domain -> (Int, Int)
-eval s d = foldr (eval' d) (0,0) s
+eval s d = undefined
 
 eval' d c (min,max) = let (m1,m2) = limits c d in (m1+min,m2+max)
 
@@ -43,24 +42,26 @@ final (_:xs) = final xs
 -- sum = w1 * v1 + w2 * v2 + w3 * v3
 -- all w positive
 sumConstraint :: Int -> [Int] -> Constraint
-sumConstraint = undefined
+sumConstraint total weights domains = map limitDomain (zip domains (reduce total weights (boundaries domains)))
 
-propagate :: Int -> [Domain] -> [Domain]
-propagate total domains = map limitDomain (zip domains (reduce total (boundaries domains)))
-
-limitDomain :: Domain -> (Int,Int) -> Domain
-limitDomain d (min,max) = undefined
+limitDomain :: (Domain,(Int,Int)) -> Domain
+limitDomain ([],_) = []
+limitDomain ((d:ds),c@(min,max)) = if d < min then limitDomain (ds,c) else
+    if d > max then [] else d : (limitDomain (ds,c))
 
 boundaries :: [Domain] -> [(Int, Int)]
 boundaries domains = map (\d -> (head d, final d)) domains
 
-reduce :: Int -> [(Int, Int)] -> [(Int,Int)]
-reduce total constraints = let
-    upper = sum $ map snd constraints
-    lower = sum $ map fst constraints
+reduce :: Int -> [Int] -> [(Int, Int)] -> [(Int,Int)]
+reduce total weights constraints = let
+    upper = weightedSum weights $ map snd constraints
+    lower = weightedSum weights $ map fst constraints
     in
-    map (\d -> (max (fst d) (total - (upper - snd d)), min (snd d) (total - (lower - fst d)))) constraints
+    map (\(w,(lo,hi)) -> (max lo (((total-upper) `quot` w)+hi),
+                          min hi (((total-lower) `quot` w)+lo))) $ zip weights constraints
 
+weightedSum :: [Int] -> [Int] -> Int
+weightedSum weights values = sum $ map (\(w,x) -> w*x) (zip weights values)
 
 -- a + b + c = 10 ; a:[1..5], b:[3..7], c[2..4]
 -- lowerLimit = 6
