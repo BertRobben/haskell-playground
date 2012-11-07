2ff02e338058dbb56349ba4025155519ac45fa12
Bert Robben <bert.robben@eu.sony.com>
2012-11-07 21:53:18 +0100

    Introduced SearchSpace, refactored modules a bit

--- search/src/Abc.hs
+++ search/src/Abc.hs
@@ -16,54 +16,28 @@ module Abc (
 
 ) where
 
-import Domain
+import Constraint.AllDifferent
+import Constraint.Sum
+import SearchSpace
+import Data.Char
 
-abcConstraint :: String -> Int -> Constraint
-abcConstraint s n d = let (min,max) = eval s d in
-    if min > n || max < n then [] else d
+exampleAbc :: SimpleSearchSpace Int
+exampleAbc = abcSearchSpace [("bonobo",44), ("condor",69), ("doejong",78),("gaviaal",142),
+    ("gibbon",51), ("gnoe",40), ("huismus",54), ("lemming",66), ("makaak",100), ("maki", 39),
+    ("narwal",127), ("oryx", 47), ("python", 60), ("quetzal", 118), ("zeelt",97)]
 
-value :: Char -> Domain -> Int
-value = undefined
+abcSearchSpace :: [(String, Int)] -> SimpleSearchSpace Int
+abcSearchSpace cons = simpleSearchSpace
+    (map (\c -> (show c,[1..26])) ['a'..'z']) -- variables
+    (((map show ['a'..'z']),newConstraint allDifferent "diff"):(map (\(s,n) -> abcConstraint s n) cons))
 
-eval :: String -> Domain -> (Int, Int)
-eval s d = foldr (eval' d) (0,0) s
+abcConstraint :: String -> Int -> ([String], Constraint Int)
+abcConstraint s n = let cc = countCharacters s in
+    (map (show . fst) cc,newConstraint (sumConstraint n $ map snd cc) s)
 
-eval' d c (min,max) = let (m1,m2) = limits c d in (m1+min,m2+max)
+countCharacters :: String -> [(Char,Int)]
+countCharacters s = foldl (\res c -> addCharacter c res) [] s
 
-limits c d = let dom = domain c d in (head dom, final dom)
-
-domain 'a' (v:_) = v
-domain c d = domain (pred c) (tail d)
-
-
-final :: [a] -> a
-final (x:[]) = x
-final (_:xs) = final xs
-
--- sum = w1 * v1 + w2 * v2 + w3 * v3
--- all w positive
-sumConstraint :: Int -> [Int] -> Constraint
-sumConstraint = undefined
-
-propagate :: Int -> [Domain] -> [Domain]
-propagate total domains = map limitDomain (zip domains (reduce total (boundaries domains)))
-
-limitDomain :: Domain -> (Int,Int) -> Domain
-limitDomain d (min,max) = undefined
-
-boundaries :: [Domain] -> [(Int, Int)]
-boundaries domains = map (\d -> (head d, final d)) domains
-
-reduce :: Int -> [(Int, Int)] -> [(Int,Int)]
-reduce total constraints = let
-    upper = sum $ map snd constraints
-    lower = sum $ map fst constraints
-    in
-    map (\d -> (max (fst d) (total - (upper - snd d)), min (snd d) (total - (lower - fst d)))) constraints
-
-
--- a + b + c = 10 ; a:[1..5], b:[3..7], c[2..4]
--- lowerLimit = 6
--- upperLimit = 16
--- lowerLimits = [10-(16-5),10-(16-7),10-(16-4)] = [-1,1,-2]
--- upperLimits = [10-(6-1),10-(6-3),10-(6-2)] = [5,7,6]
+addCharacter :: (Eq a) => a -> [(a,Int)] -> [(a,Int)]
+addCharacter a [] = [(a,1)]
+addCharacter a ((c,n):rest) = if (c == a) then (c,n+1):rest else (c,n):(addCharacter a rest)
--- search/src/Main.hs
+++ search/src/Main.hs
@@ -17,9 +17,13 @@ module Main (
     main
 ) where
 
+import SearchSpace
 import Domain
-import AllDifferent
+import Constraint.AllDifferent
 import Abc
+import Sudoku
+import Anagram
+import Constraint.Sum
 import qualified Data.Map as Map
 
 data SampleSpace = SampleSpace { one::[Int], two::[Int], three::[Int] } deriving Show
